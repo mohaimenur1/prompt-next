e79b3c91e3a1793b778b7bfae44384ad7e02a26d
Mohai Menur <mmrahmanrahi@gmail.com>
2023-05-23 18:19:07 +0600

    finishing up the starter

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h2>Home page</h2>
+    </main>
+  );
+}
