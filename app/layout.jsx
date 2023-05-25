4534f2e3bdbc8735bffe0226fbfeb888d0d0adcb
mohaimenur1 <mmrahmanrahi@gmail.com>
2023-05-25 12:11:09 +0600

    adding next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,27 @@
 /** @format */
 
-import Nav from '@components/Nav';
-import '@styles/globals.css';
+import Nav from "@components/Nav";
+import "@styles/globals.css";
+import Provider from "@components/Provider";
 
 export const metadata = {
-  title: 'Promptopia',
-  description: 'Discover & Share AI Prompts',
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
